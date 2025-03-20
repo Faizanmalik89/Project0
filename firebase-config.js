e69da6a2523f33f97d9f781a067fb86fc7b0489b
MALIK <134768946+Faizanmalik89@users.noreply.github.com>
2025-03-20 23:04:18 +0500

    Update firebase-config.js

--- firebase-config.js
+++ firebase-config.js
@@ -1,9 +1,9 @@
 const firebaseConfig = {
-    apiKey: "YOUR_FIREBASE_API_KEY",
-    authDomain: "YOUR_FIREBASE_AUTH_DOMAIN",
-    projectId: "YOUR_FIREBASE_PROJECT_ID",
-    storageBucket: "YOUR_FIREBASE_STORAGE_BUCKET",
-    messagingSenderId: "YOUR_FIREBASE_MESSAGING_SENDER_ID",
-    appId: "YOUR_FIREBASE_APP_ID"
+    apiKey: "AIzaSyAeopZBOH519qVwLkAL06RttMG-wyfAC5M",
+    authDomain: "realistic-c5616.firebaseapp.com",
+    projectId: "realistic-c5616",
+    storageBucket: "realistic-c5616.firebasestorage.app",
+    messagingSenderId: "809481702160",
+    appId: "1:809481702160:web:10a82f267efb5dcc137395"
 };
 firebase.initializeApp(firebaseConfig);
